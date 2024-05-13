eb1648d171773420cf55f6c4b0a1666fa990cf52
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-13 03:58:23 -0400

    Update Departamento.java
    Quite el método toString, porque como el dato es un String el getter cumple la misma función.

--- Departamento.java
+++ Departamento.java
@@ -19,8 +19,4 @@ public class Departamento {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    /**@Override*/
-    public String toString() {
-        return nombre;
-    }
 }
