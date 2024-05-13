6edbee2f58262c330a939d801354bddbd69aef31
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-13 17:52:34 -0400

    Update ReunionVirtual.java
    Actualizado el constructor para establecer también las variables de la clase abstract Reunion.

--- ReunionVirtual.java
+++ ReunionVirtual.java
@@ -1,12 +1,20 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
 /**Reunión virtual asociada a un enlace
  * @author Gustavo González
- * @version versión 2, 13 de mayo 2024*/
+ * @version versión 3, 13 de mayo 2024*/
 public class ReunionVirtual extends Reunion {
     /**String para almacenar el enlace de la reunión*/
     private String enlace;
-    /**Constructor de ReunionVirtual, recibe el enlace de la reunión y lo asigna al parámetro
+    /**Constructor de ReunionVirtual, recibe y asigna; la fecha de la reunión, la hora prevista, la duración prevista y el enlace de la reunión
+     * @param fecha Date
+     * @param horaPrevista Instant
+     * @param duracionPrevista Duration
      * @param enlace String*/
-    public ReunionVirtual(String enlace) {
+    public ReunionVirtual(Date fecha, Instant horaPrevista, Duration duracionPrevista, String enlace) {
+        super(fecha, horaPrevista, duracionPrevista);
         this.enlace = enlace;
     }
     /**Getter del enlace de la reunión
