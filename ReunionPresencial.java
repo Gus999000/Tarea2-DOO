7922568e955df704d8c466128e13baafb3721163
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-13 17:43:03 -0400

    Update ReunionPresencial.java
    Actualizado el constructor para establecer también las variables de la clase abstract Reunion.

--- ReunionPresencial.java
+++ ReunionPresencial.java
@@ -1,12 +1,20 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
 /**Reunión presencial en una sala
  * @author Gustavo González
- * @version versión 2, 13 de mayo 2024*/
+ * @version versión 3, 13 de mayo 2024*/
 public class ReunionPresencial extends Reunion {
     /**String para almacenar la sala de la reunión*/
     private String sala;
-    /**Constructor de ReunionPresencial, recibe la sala de la reunión y la asigna al parámetro
+    /**Constructor de ReunionPresencial, recibe y asigna; la fecha de la reunión, la hora prevista, la duración prevista y la sala de la reunión
+     * @param fecha Date
+     * @param horaPrevista Instant
+     * @param duracionPrevista Duration
      * @param sala String*/
-    public ReunionPresencial(String sala) {
+    public ReunionPresencial(Date fecha, Instant horaPrevista, Duration duracionPrevista, String sala) {
+        super(fecha, horaPrevista, duracionPrevista);
         this.sala = sala;
     }
     /**Getter de la sala de la reunión
