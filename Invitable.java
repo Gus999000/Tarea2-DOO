aa1f440a458ed26cb7601a9405e6c53be2c5c654
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-14 01:33:44 -0400

    Create Invitable.java

---
+++ Invitable.java
@@ -0,0 +1,8 @@
+/**Interface Invitable para incluir el método invitar
+ * @author Gustavo Benítez
+ * @version versión 1, 14 de mayo 2024*/
+public interface Invitable {
+    /**Implementa el método invitar
+     * @return la invitación a la reunión*/
+    public Invitacion invitar();
+}
