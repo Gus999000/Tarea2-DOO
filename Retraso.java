2d800e9764620577f2f0a8470985fbdcfe316a5f
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-14 03:58:17 -0400

    Update Retraso.java
    Implementación de la super clase Asistencia y actualización del método toString().

--- Retraso.java
+++ Retraso.java
@@ -1,12 +1,15 @@
 /**Clase para registrar retrasos
  * @author Gustavo González
- * @version versión 3, 12 de mayo 2024*/
+ * @version versión 4, 14 de mayo 2024*/
 import java.time.Instant;
-public class Retraso {
+import java.time.format.DateTimeFormatter;
+
+public class Retraso extends Asistencia {
     /**Instant para almacenar la hora del retraso*/
     private Instant hora;
-    /**Constructor de Retraso, usa Instant.now() para definir la hora en el instante de tiempo*/
-    public Retraso() {
+    /**Constructor de Retraso, deja al empleado presente y usa Instant.now() para definir la hora del retraso*/
+    public Retraso(Empleado E) {
+        super(E);
         this.hora = Instant.now();
     }
     /**Getter de la hora del retraso
@@ -19,8 +22,8 @@ public class Retraso {
     public void setHora(Instant hora) {
         this.hora = hora;
     }
-    /**@Override*/
+    @Override
     public String toString() {
-        return String.valueOf(hora);
+        return "Asistencia: " + Boolean.toString(super.getAsistencia()) + "Hora del retraso: " + DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss").format(hora);
     }
 }
