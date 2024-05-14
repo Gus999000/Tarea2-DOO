0935a6eee159d09b43063b7da502f7891cd3c6de
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-14 07:19:27 -0400

    Update Invitable.java
    Modificación a invitar().

--- Invitable.java
+++ Invitable.java
@@ -1,8 +1,8 @@
 /**Interface Invitable para incluir el método invitar
  * @author Gustavo Benítez
- * @version versión 1, 14 de mayo 2024*/
+ * @version versión 2, 14 de mayo 2024*/
 public interface Invitable {
     /**Implementa el método invitar
      * @return la invitación a la reunión*/
-    public Invitacion invitar();
+    public Invitacion invitar(Reunion X);
 }
