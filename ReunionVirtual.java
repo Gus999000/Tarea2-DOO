897b0c8a716d93dff8d4fd6d0025e58c77dcf25d
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-14 10:23:58 -0400

    Update ReunionVirtual.java
    Implementado el método para crear el informe y su documentación.

--- ReunionVirtual.java
+++ ReunionVirtual.java
@@ -1,9 +1,14 @@
+/**Reunión virtual asociada a un enlace
+ * @author Gustavo González
+ * @version versión 6, 14 de mayo 2024
+ * @see java.io.IOException*/
+import java.io.BufferedWriter;
 import java.util.Date;
 import java.time.Instant;
 import java.time.Duration;
-/**Reunión virtual asociada a un enlace
- * @author Gustavo González
- * @version versión 5, 14 de mayo 2024*/
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class ReunionVirtual extends Reunion {
     /**String para almacenar el enlace de la reunión*/
     private String enlace;
@@ -17,6 +22,26 @@ public class ReunionVirtual extends Reunion {
     public ReunionVirtual(Empleado organizador, String tipoReunion, Date fecha, Instant horaPrevista, Duration duracionPrevista, String enlace) {
         super(organizador, tipoReunion, fecha, horaPrevista, duracionPrevista);
         this.enlace = enlace;
+    }
+    /**Método para crear el Informe como un archivo de texto
+     * @throws IOException lanza esta excepción si ocurre un error al crear el archivo*/
+    public void crearInforme() throws IOException {
+        try {
+            File Informe = new File("Informe.txt");
+            FileWriter escrituraArchivo = new FileWriter(Informe);
+            BufferedWriter escrituraBuffer = new BufferedWriter(escrituraArchivo);
+            escrituraBuffer.write("Fecha: " + fecha.toString() + "\n" + "Hora de la reunión: " + horaPrevista.toString() + "\n" + "Hora de inicio: " + horaInicio.toString() + "\n" + "Hora de fin: " + horaFin.toString() + "\n" + "Duración total: " + this.calcularTiempoReal() + "\n" + "Tipo de reunión: " + tipoReunion + "\n" + "Enlace: " + enlace + "\n");
+            for(int i = 0; i < Invitados.size(); i = i + 1) {
+                escrituraBuffer.write(Invitados.get(i).toString() + "\n");
+            }
+            for(int i = 0; i < Notas.size(); i = i + 1) {
+                escrituraBuffer.write(Notas.get(i).getContenido() + "\n");
+            }
+            escrituraBuffer.close();
+        }
+        catch(IOException exception) {
+            System.out.println("Error al crear archivo");
+        }
     }
     /**Getter del enlace de la reunión
      * @return enlace de la reunión*/
