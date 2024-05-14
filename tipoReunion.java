4fc221b07dfe8522e7e6b90b1b1d612568a9c74b
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-14 04:57:48 -0400

    Update tipoReunion.java
    Añadido constructor, getter y toString.

--- tipoReunion.java
+++ tipoReunion.java
@@ -1,8 +1,24 @@
 /**Enumeración de los 3 tipos principales de reuniones
  * @author Gustavo González
- * @version versión 2, 12 de mayo 2024*/
+ * @version versión 3, 14 de mayo 2024*/
 enum tipoReunion {
-    TECNICA,
-    MARKETING,
-    OTRO;
+    TECNICAS("técnicas"),
+    MARKETING("marketing"),
+    OTROS("otros");
+    /**String indicando el tipo de reunión*/
+    private final String tipoReunion;
+    /**Constructor de la enumeración
+     * @param tipoReunion String*/
+    tipoReunion(String tipoReunion) {
+        this.tipoReunion = tipoReunion;
+    }
+    /**Getter del tipo de reunión
+     * @return tipo de reunión*/
+    public String getTipoReunion() {
+        return tipoReunion;
+    }
+    @Override
+    public String toString() {
+        return "Tipo de la reunión: " + tipoReunion;
+    }
 }
