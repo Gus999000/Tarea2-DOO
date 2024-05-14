6c14aa8c88c2f1f92f8507cb444796adc2742a21
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-14 06:15:11 -0400

    Update ReunionPresencial.java
    Añadidos parámetros de super clase.

--- ReunionPresencial.java
+++ ReunionPresencial.java
@@ -3,17 +3,19 @@ import java.time.Instant;
 import java.time.Duration;
 /**Reunión presencial en una sala
  * @author Gustavo González
- * @version versión 4, 14 de mayo 2024*/
+ * @version versión 5, 14 de mayo 2024*/
 public class ReunionPresencial extends Reunion {
     /**String para almacenar la sala de la reunión*/
     private String sala;
-    /**Constructor de ReunionPresencial, recibe y asigna; la fecha de la reunión, la hora prevista, la duración prevista y la sala de la reunión
+    /**Constructor de ReunionPresencial, recibe y asigna; el organizador, el tipo de reunión, la fecha de la reunión, la hora prevista, la duración prevista y la sala de la reunión
+     * @param organizador Empleado
+     * @param tipoReunion String
      * @param fecha Date
      * @param horaPrevista Instant
      * @param duracionPrevista Duration
      * @param sala String*/
-    public ReunionPresencial(Date fecha, Instant horaPrevista, Duration duracionPrevista, String sala) {
-        super(fecha, horaPrevista, duracionPrevista);
+    public ReunionPresencial(Empleado organizador, String tipoReunion, Date fecha, Instant horaPrevista, Duration duracionPrevista, String sala) {
+        super(organizador, tipoReunion, fecha, horaPrevista, duracionPrevista);
         this.sala = sala;
     }
     /**Getter de la sala de la reunión
