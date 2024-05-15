6bfc91d2ae6431e0479ffd69ee3f9702c8681f91
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-15 03:00:49 -0400

    Update DepartamentoTest.java

--- DepartamentoTest.java
+++ DepartamentoTest.java
@@ -1,6 +1,12 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +22,20 @@ class DepartamentoTest {
 
     @Test
     void invitar() {
+        Empleado E = new Empleado("25", "De La Barrera Ortiz", "Juan", "hola@gmail.com", true);
+        Empleado E1 = new Empleado("55", "Miranda Ortiz", "Franco", "chao@gmail.com", false);
+        Empleado E2 = new Empleado("69", "Cartes Ortiz", "Patricio", "ortiz@gmail.com", true);
+        Departamento D = new Departamento("Recursos humanos");
+        D.addMiembros(E);
+        D.addMiembros(E1);
+        D.addMiembros(E2);
+        Date F = new Date();
+        Instant HP = Instant.now();
+        Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
+        ReunionPresencial P = new ReunionPresencial(E, tipoReunion.MARKETING, F, HP, DP, "420");
+        D.invitar(P);
+        int I = P.obtenerTotalAsistencia();
+        final int X = 2;
+        Assertions.assertEquals(I, X);
     }
 }
