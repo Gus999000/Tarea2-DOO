b6535fde02d0ce1257fbc96fed71b2894e32f188
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-15 03:48:34 -0400

    Update ReunionTest.java

--- ReunionTest.java
+++ ReunionTest.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,14 +23,50 @@ class ReunionTest {
 
     @Test
     void obtenerAsistencias() {
+        Empleado E = new Empleado("69", "Kasuga Arakawa", "Ichiban", "dulcesichiban777@gmail.com", false);
+        Empleado E1 = new Empleado("2", "Morgan De La Barrera", "Arthur", "redenciónmuertaroja@gmail.com", true);
+        Date F = new Date();
+        Instant HP = Instant.now();
+        Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "github.com");
+        E.invitar(P);
+        E1.invitar(P);
+        List L = P.obtenerAsistencias();
+        Empleado X = (Empleado) L.get(0);
+        Assertions.assertEquals(E1, X);
     }
 
     @Test
     void obtenerAusencias() {
+        Empleado E = new Empleado("69", "Kasuga Arakawa", "Ichiban", "dulcesichiban777@gmail.com", false);
+        Empleado E1 = new Empleado("2", "Morgan De La Barrera", "Arthur", "redenciónmuertaroja@gmail.com", true);
+        Date F = new Date();
+        Instant HP = Instant.now();
+        Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "github.com");
+        E.invitar(P);
+        E1.invitar(P);
+        List L = P.obtenerAusencias();
+        Empleado X = (Empleado) L.get(0);
+        Assertions.assertEquals(E, X);
     }
 
     @Test
     void obtenerRetrasos() {
+        Empleado E = new Empleado("69", "Kasuga Arakawa", "Ichiban", "dulcesichiban777@gmail.com", false);
+        Empleado E1 = new Empleado("2", "Morgan De La Barrera", "Arthur", "redenciónmuertaroja@gmail.com", true);
+        Date F = new Date();
+        Instant HP = Instant.now();
+        Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "github.com");
+        E.invitar(P);
+        E1.invitar(P);
+        List L = P.obtenerAusencias();
+        Empleado X = (Empleado) L.get(0);
+        X.setPresencia(true);
+        List L1 = P.obtenerRetrasos();
+        Empleado X1 = (Empleado) L1.get(0);
+        Assertions.assertTrue(X1.getPresencia());
     }
 
     @Test
@@ -38,7 +75,7 @@ class ReunionTest {
         Date F = new Date();
         Instant HP = Instant.now();
         Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
-        ReunionPresencial P = new ReunionPresencial(E, tipoReunion.MARKETING, F, HP, DP, "420");
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "github.com");
         E.invitar(P);
         int I = P.obtenerTotalAsistencia();
         final int X = 1;
@@ -51,15 +88,24 @@ class ReunionTest {
         Date F = new Date();
         Instant HP = Instant.now();
         Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
-        ReunionPresencial P = new ReunionPresencial(E, tipoReunion.MARKETING, F, HP, DP, "420");
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "github.com");
         E.invitar(P);
         float I = P.obtenerPorcentajeAsistencia();
-        final int X = 1;
+        final float X = 1 / 100;
         Assertions.assertEquals(I, X);
     }
 
     @Test
     void calcularTiempoReal() {
+        Empleado E = new Empleado("50", "De La Barrera Ortiz", "Juan", "hola@gmail.com", true);
+        Date F = new Date();
+        Instant HP = Instant.now();
+        Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "github.com");
+        E.invitar(P);
+        float I = P.obtenerPorcentajeAsistencia();
+        final float X = 0;
+        Assertions.assertEquals(I, X);
     }
 
     @Test
@@ -68,7 +114,7 @@ class ReunionTest {
         Date F = new Date();
         Instant HP = Instant.now();
         Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
-        ReunionPresencial P = new ReunionPresencial(E, tipoReunion.MARKETING, F, HP, DP, "420");
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "420");
         P.iniciar();
         Instant I = P.getHoraInicio();
         final Instant X = P.getHoraInicio();
@@ -81,7 +127,7 @@ class ReunionTest {
         Date F = new Date();
         Instant HP = Instant.now();
         Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
-        ReunionPresencial P = new ReunionPresencial(E, tipoReunion.MARKETING, F, HP, DP, "420");
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "420");
         P.finalizar();
         Instant I = P.getHoraFin();
         final Instant X = P.getHoraFin();
@@ -94,7 +140,7 @@ class ReunionTest {
         Date F = new Date();
         Instant HP = Instant.now();
         Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
-        ReunionPresencial P = new ReunionPresencial(E, tipoReunion.MARKETING, F, HP, DP, "420");
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "420");
         Nota I = P.crearNota("Hola mundo");
         final Nota X = new Nota("Hola mundo");
         Assertions.assertEquals(I, X);
