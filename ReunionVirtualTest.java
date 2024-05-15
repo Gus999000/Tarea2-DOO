3c46048f86be6d85a27f477fa10238429edbae10
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-15 04:21:19 -0400

    Update ReunionVirtualTest.java

--- ReunionVirtualTest.java
+++ ReunionVirtualTest.java
@@ -1,10 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReunionVirtualTest {
 
     @Test
-    void crearInforme() {
+    void crearInforme() throws IOException {
+        Empleado E = new Empleado("69", "Kasuga Arakawa", "Ichiban", "dulcesichiban777@gmail.com", true);
+        Date F = new Date();
+        Instant HP = Instant.now();
+        Duration DP = Duration.between(LocalTime.of(15, 10), LocalTime.of(18, 15));
+        ReunionVirtual P = new ReunionVirtual(E, tipoReunion.MARKETING, F, HP, DP, "github.com");
+        P.iniciar();
+        P.finalizar();
+        P.crearInforme();
     }
 }
