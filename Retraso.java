fff4a48d38eac7d6868201a12f4e37d7a95bfbf3
Gus999000 <147113241+Gus999000@users.noreply.github.com>
2024-05-15 00:35:15 -0400

    Update Retraso.java
    Actualizado el método toString().

--- Retraso.java
+++ Retraso.java
@@ -1,6 +1,6 @@
 /**Clase para registrar retrasos
  * @author Gustavo González
- * @version versión 4, 14 de mayo 2024*/
+ * @version versión 5, 15 de mayo 2024*/
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 
@@ -24,6 +24,6 @@ public class Retraso extends Asistencia {
     }
     @Override
     public String toString() {
-        return "Asistencia: " + Boolean.toString(super.getAsistencia()) + "Hora del retraso: " + DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss").format(hora);
+        return "Asistencia: " + Boolean.toString(super.getAsistencia()) + "\n" + "Hora del retraso: " + DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss").format(hora);
     }
 }
